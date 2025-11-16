6b89a76f462301f482925c367bcd0cb777f40697
ferreirascdb <cruzrikjr@hotmail.com>
2025-11-16 20:03:27 -0300

    Index atualizado sem js, faltando editar e delete

--- project/candidatos/views.py
+++ project/candidatos/views.py
@@ -1,4 +1,24 @@
 from django.shortcuts import render
 
+from candidatos.models import Candidato
+
+
+
 def index(request):
-    return render(request, 'candidatos/index.html')
+    loja = request.GET.get('loja')
+    busca = request.GET.get('busca')
+
+    candidatos = Candidato.objects.all()
+
+    if loja:
+        candidatos = candidatos.filter(loja=loja)
+
+    if busca:
+        candidatos = candidatos.filter(nome__icontains=busca)
+
+    lojas = Candidato.objects.values_list('loja', flat=True).distinct()
+
+    return render(request, 'candidatos/index.html', {
+        "candidatos": candidatos,
+        "lojas": lojas,
+    })
